dc50c290a56541760cf35b776e1881c7ca60ba8a
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-02 15:12:39 +0700

    edit store dan reducer

--- src/app/store.js
+++ src/app/store.js
@@ -3,11 +3,9 @@
 
 import { configureStore } from '@reduxjs/toolkit';
 // import counterReducer from '../features/counter/counterSlice';
-import productReducer from '../features/productSlice';
 
 export const store = configureStore({
   reducer: {
     // counter: counterReducer,
-    product: productReducer,
   },
 });
