6e94c21a27fa9d6185ef48efa82cafa5e2b7e1d6
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-02 14:56:15 +0700

    Tambah komentar untuk file store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,3 +1,6 @@
+// > Store
+// => Digunakan untuk menampung state dari sebuah aplikasi
+
 import { configureStore } from '@reduxjs/toolkit';
 // import counterReducer from '../features/counter/counterSlice';
 import productReducer from '../features/productSlice';
