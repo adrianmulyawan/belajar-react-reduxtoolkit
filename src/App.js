d91fff565994e6c5f6af765080336b6733d932c9
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-02 13:51:34 +0700

    tambah route untuk halaman product

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/home.page';
+import ProductPage from './pages/product.page';
 // import logo from './logo.svg';
 // import { Counter } from './features/counter/Counter';
 // import './App.css';
@@ -11,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={ <HomePage /> } />
+          <Route path='/product' element={ <ProductPage /> } />
         </Routes>
       </BrowserRouter>
     </>
