c9f923baca7033f5c4ae8143db829f6b0ed6f2cd
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-02 19:15:25 +0700

    buat state untuk menampung data product, dan buat event saat form di submbit

--- src/components/Product/addProduct.component.jsx
+++ src/components/Product/addProduct.component.jsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddProductComponent = () => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+  
+  const handleFormAddProduct = (event) => {
+    event.preventDefault();
+
+    console.info(title, price, description, '=> ini adalah datanya');
+  }
+
   return (
     <>
       <h3>Add New Product</h3>
       <hr />
       <div className="card p-3">
-        <form>
+        <form onSubmit={ handleFormAddProduct }>
           <div className="mb-3">
             <label htmlFor="title" className="form-label">Title</label>
-            <input type="text" className="form-control" id="title" name='title' placeholder='Input Product Name' required />
+            <input type="text" className="form-control" id="title" name='title' value={ title } onChange={ (event) => setTitle(event.target.value) } placeholder='Input Product Name' required />
           </div>
-          <div className="mb-3">
+          <div className="msb-3">
             <label htmlFor="description" className="form-label">Description</label>
-            <input type="text" className="form-control" id="description" name='description' placeholder='Input Description Product' required />
+            <input type="text" className="form-control" id="description" name='description' value={ description } onChange={ (event) => setDescription(event.target.value) } placeholder='Input Description Product' required />
           </div>
           <div className="mb-3">
             <label htmlFor="price" className="form-label">Price</label>
-            <input type="text" className="form-control" id="price" name='price' placeholder='Input Product Price' required />
+            <input type="text" className="form-control" id="price" name='price' value={ price } onChange={ (event) => setPrice(event.target.value) } placeholder='Input Product Price' required />
           </div>
           <div className="d-grid gap-2">
             <button type="submit" className="btn btn-primary">
