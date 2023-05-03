29c2e3d5bedf1c37c01da41bc6cd81956862f68b
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-03 11:49:55 +0700

    edit tampilan halaman product

--- src/pages/product.page.jsx
+++ src/pages/product.page.jsx
@@ -9,16 +9,16 @@ const ProductPage = () => {
       <NavbarComponent />
       <div className="container mt-3">
         <div className="row justify-content-center">
-          {/* Add Product */}
-          <div className="col-sm-12 col-md-6 col-lg-7">
-            <div className="card p-3">
-              <AddProductComponent />
-            </div>
-          </div>
           {/* Show Product */}
           <div className="col-sm-12 col-md-6 col-lg-5">
             <div className="card p-3">
               <ShowProductComponent />
+            </div>
+          </div>
+          {/* Add Product */}
+          <div className="col-sm-12 col-md-6 col-lg-7">
+            <div className="card p-3">
+              <AddProductComponent />
             </div>
           </div>
         </div>
