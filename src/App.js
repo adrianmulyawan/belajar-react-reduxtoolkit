df9ecf6ec5dacc9c073f5ae380f4a3edb361ab0d
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-03 11:57:46 +0700

    tambah route untuk halaman not found

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/home.page';
 import ProductPage from './pages/product.page';
 import EditProductPage from './pages/editProduct.page';
+import NotFoundPage from './pages/notFound.page';
 // import logo from './logo.svg';
 // import { Counter } from './features/counter/Counter';
 // import './App.css';
@@ -15,6 +16,7 @@ function App() {
           <Route path='/' element={ <HomePage /> } />
           <Route path='/product' element={ <ProductPage /> } />
           <Route path='/product/edit/:id' element={ <EditProductPage /> } />
+          <Route path='*' element={ <NotFoundPage /> } />
         </Routes>
       </BrowserRouter>
     </>
