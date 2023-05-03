930f5e5005b085f70c2ad571399be84078bb2662
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-03 22:23:38 +0700

    implement update product di component update

--- src/components/Product/editProduct.component.jsx
+++ src/components/Product/editProduct.component.jsx
@@ -1,12 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getProducts, productSelector, updateProduct } from '../../features/productSlice';
+import { useParams, useNavigate } from 'react-router-dom';
 
 const EditProductComponent = () => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [price, setPrice] = useState('');
 
-  const handleFormUpdateProduct = (event) => {
+  // > navigate (direct kehalaman tertentu)
+  const navigate = useNavigate();
+  // > ambil parameter 'id' menggunakan useParams
+  const {id} = useParams();
+
+  // > dispatch (update state)
+  const dispatch = useDispatch();
+  // > ambil data dari state menggunakan useSelector
+  // => tapi satu data saja yang akan diambil
+  const product = useSelector((state) => productSelector.selectById(state, id));
+
+  let i = 0;
+  // > useEffect untuk ambil data product
+  useEffect(() => {
+    if (i === 0) {
+      dispatch(getProducts());
+      i++;
+    }
+  }, [dispatch, i]);
+
+  let j = 0;
+  // > useEffect umtuk set 'value' pada form
+  useEffect(() => {
+    if (j === 0) {
+      if (product) {
+        setTitle(product.title);
+        setDescription(product.description);
+        setPrice(product.price);
+      }
+      j++;
+    }
+  }, [product, j]);
+
+  // > data yang akan diupdate
+  const data = {
+    id,
+    title,
+    description,
+    price
+  };
+
+  const handleFormUpdateProduct = async (event) => {
     event.preventDefault();
+
+    await dispatch(updateProduct(data));
+
+    navigate('/product');
   } 
 
   return (
