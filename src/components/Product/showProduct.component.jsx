0820d205467a19f8c2386769b2cd463941d88da8
adrianmulyawan <adrianmulyawan666@gmail.com>
2023-05-03 14:21:32 +0700

    tambah logic untuk memanggil data product dari api

--- src/components/Product/showProduct.component.jsx
+++ src/components/Product/showProduct.component.jsx
@@ -1,30 +1,49 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, {  useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getProducts, productSelector } from '../../features/productSlice';
 
 const ShowProductComponent = () => {
   // > akses state dari store.js (./app/store.js)
   // => menggunakan hooks useSelector(): untuk mendapatkan data dari 'state' yang berada di 'store'.
-  const { 
-    title, description, price
-  } = useSelector((state) => state.product);
+  // const { 
+  //   title, description, price
+  // } = useSelector((state) => state.product);
+
+  const dispatch = useDispatch();
+  const products = useSelector(productSelector.selectAll);
+  
+  let i = 0;
+  useEffect(() => {
+    if (i === 0) {
+      dispatch(getProducts())
+    }
+  }, [dispatch, i]);
 
   return (
     <>
       <h3>Show All Products</h3>
       <hr />
-      <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">
-            { title }
-          </h5>
-          <p className="card-text">
-            { description }
-          </p>
-          <p className='text-muted'>
-            Rp { price },00
-          </p>
-        </div>
-      </div>
+      {
+        products.map((product) => {
+          return (
+            <div key={ product.id } className="card mb-2">
+              <div className="card-body">
+                <h5 className="card-title">
+                  { product.title }
+                </h5>
+                <p className="card-text">
+                  { product.description }
+                </p>
+                <p className='text-muted'>
+                  Rp { product.price },00
+                </p>
+                <button type="submit" className="btn btn-primary">Edit</button>
+                <button type="submit" className="btn btn-danger mx-2">Delete</button>
+              </div>
+            </div>
+          )
+        })
+      }
     </>
   );
 }
